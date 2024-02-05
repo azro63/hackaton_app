4ac7eda388d35a1d3024a0362e6c412c101500ce
azro63 <bogdantogun@gmail.com>
2024-02-05 17:56:57 +0400

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hackaton_app"
+include(":app")
